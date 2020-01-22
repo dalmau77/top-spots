72e5a1f001ae0cc4a723ab2a012a0664a6718095
Eric Dalmau <dalmaueric29@gmail.com>
2020-01-22 13:11:29 -0800

    i really enjoyed this project

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-$(document).ready(function() {
-// write your code here
-
+$(document).ready(function () {
+    // write your code here
+    $.getJSON("data.json", function (data) {
+        var newData = '';
+        $.each(data, function (item, index) {
+            newData += '<tr>';
+            newData += '<td>' + index.name;
+            newData += '<td>' + index.description + '</td>';
+            newData += '<td><a href="https://www.google.com/maps?q=' + index.location + '">' + "open in google" + '</a></td>';
+            newData += '</tr>';
+        });
+        $('#content').append(newData);
+        console.log(newData);
+    });
 });
+
